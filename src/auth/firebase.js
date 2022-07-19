d94b7ca6e9f2a508c3772fc778113db8468f7a27
yaser <ddemety372@gmail.com>
2022-07-19 22:00:12 +0300

    fireBase...

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,1 +1,20 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  apiKey: "AIzaSyBCS39RLJYE09EU0XwghcsQLjkxVLeuYj4",
+  authDomain: "movieappreact-6b07b.firebaseapp.com",
+  projectId: "movieappreact-6b07b",
+  storageBucket: "movieappreact-6b07b.appspot.com",
+  messagingSenderId: "272107674981",
+  appId: "1:272107674981:web:3e96e06110ed4f8ae7d58c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import {useNavigate} from 'react-router-dom'
 
 const Navbar = () => {
+  const navigate = useNavigate()
+  // const currentUser = {
+  //   displayName: "Yaser Demet",
+  // };
+  const currentUser = false
   return (
     <div>
-      <p>Navbar</p>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container fluid">
+          <Link to="/" className="navbar-brand text-white">
+            <h4 className="navbar-header">React Movie App</h4>
+          </Link>
+
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+                {" "}
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser.displayName}
+                </h5>
+                <button className="ms-2 btn btn-dark btn-outline-light">Logout</button>
+              </>
+            ) : (
+              <>
+                <button className="ms-2 btn btn-dark btn-outline-light" onClick={() => navigate("/login")}>Login</button>
+
+                <button className="ms-2 btn btn-dark btn-outline-light" onClick={() => navigate("/register")}>Register</button>
+              </>
+            )}
+          </div>
+        </div>
+      </nav>
     </div>
   );
 };
